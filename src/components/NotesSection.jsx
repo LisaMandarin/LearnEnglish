e78fdcbfc8c58ab6d757574adab8ba01238f64cc
LisaMandarin <chu23001@byui.edu>
2024-07-28 04:25:45 +0800

    working on GeneratePDF

--- src/components/NotesSection.jsx
+++ src/components/NotesSection.jsx
@@ -26,7 +26,6 @@ export function NotesSection({notes, setNotes, translationAreaRef}) {
     }
 
     const GeneratePDF = () => {
-        
         if (translationAreaRef.current) {
             // ----- basic pdf setting -----
             const pdf = new jsPDF({lineHeight: 1})  // default unit: mm
@@ -49,6 +48,45 @@ export function NotesSection({notes, setNotes, translationAreaRef}) {
             const translationContent = translationAreaRef.current.innerText;
             const splitTranslation = pdf.splitTextToSize(translationContent, maxWidth)  // split text to fit within the maxWidth
             console.log('Translation: ', splitTranslation)
+
+            let currentY = margins.top;  // starting Y for translation header
+            pdf.text('Translation: ', margins.left, currentY)  // add header
+            currentY += 10;  // starting Y for translation content
+
+            for (let line of splitTranslation) {
+                if (currentY + 10 > pageHeight - margins.bottom) {  // ensure enough space for new content
+                    pdf.addPage();
+                    currentY = margins.top;  // reset margin top for new page
+                }
+                pdf.text(line, margins.left, currentY)  // output translation content
+                currentY += 10  // increase by line height
+            }
+            // ----- end of generate translation section -----
+
+            currentY += 5; // gap between sections
+
+            // ----- generate notes section -----
+            if (currentY + 10 > pageHeight - margins.bottom) {  // ensure enough space for new content
+                pdf.addPage();
+                currentY = margins.top;  // reset margin top for new page
+            }
+            pdf.text('Note: ', margins.left, currentY);
+            currentY += 10;  // staring Y for notes content
+
+            const notesContent = textAreaRef.current.innerText;
+            const splitNotes = pdf.splitTextToSize(notesContent, maxWidth);  // split text to fit within the maxWidth
+            console.log('notes: ', splitNotes)
+            for (let line of splitNotes) {
+                if (currentY + 10 > pageHeight - margins.bottom) {
+                    pdf.addPage();
+                    currentY = margins.top;
+                }
+                pdf.text(line, margins.left, currentY);
+                currentY += 10;
+            }
+            // ----- end of generate notes section -----
+
+            pdf.save('句句通.pdf');
         } else {
             console.log('translationAreaRef not existing')
         }
