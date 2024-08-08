ff7d9ae20bbbc5e1771986804ec2f2098a2c7d7b
LisaMandarin <chu23001@byui.edu>
2024-08-08 11:11:34 +0800

    delete comments

--- src/components/APIs/openai.js
+++ src/components/APIs/openai.js
@@ -4,7 +4,6 @@ export async function openAIResult (termChinese, termEnglish, termExample, selec
     const displayResult = async (word, lookupTerms) => {
         const openai = new OpenAI({
             apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-            // apiKey: process.env.VITE_OPENAI_API_KEY,
             dangerouslyAllowBrowser: true
         })
         const instructions = `You are an English-Chinese(traditional) dictionary for English learners.  Word: ${word}.  Tell me the word's information of ${lookupTerms.join(',')} and its part of speech`
